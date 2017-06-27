ff77bafae545bba213bc1c01943608755bf7afb6
Enoch Ko <enoch232@gmail.com>
2017-06-26 20:21:50 -0700

    make calculator console scroll to bottom automatically

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -10,9 +10,14 @@ import {
 export default class Calculator extends Component {
   constructor(props){
     super(props)
+    this.scrollView = null
     this.state = {
       expression: ""
     }
+  }
+
+  componentsDidMount(){
+    this.scrollView.scrollToEnd({animated: true})
   }
 
   _evaluateExpression(){
@@ -23,13 +28,15 @@ export default class Calculator extends Component {
   render(){
     return (
       <View style = {{alignItems: 'center', height: '100%', width: '100%'}}>
-        <View style = {{height: '20%', width: '100%', backgroundColor: '#EFEFEF', flexDirection: 'row', alignItems: 'center'}}>
+        <View style = {{height: '10%', width: '100%', backgroundColor: '#EFEFEF', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
           <Text style = {{fontSize: 30}}>Calculator</Text>
-          <View style = {{backgroundColor: '#FFC742', height: '30%', marginHorizontal: 20}}>
+          <TouchableOpacity style = {{backgroundColor: '#FFC742', paddingHorizontal: 10, marginHorizontal: 30, borderRadius: 4}}>
             <Text style = {{fontSize: 30}}>RPN</Text>
-          </View>
+          </TouchableOpacity>
         </View>
-        <ScrollView style = {{backgroundColor: '#2B3D50', height: '80%', width: '100%'}}>
+        <ScrollView
+           ref={(scrollView) => {this.scrollView = scrollView}} onContentSizeChange={(contentWidth, contentHeight) => {this.scrollView.scrollToEnd({animated: true})}}
+           style = {{backgroundColor: '#2B3D50', height: '80%', width: '100%'}}>
           {this.props.calculator.history.map((result, index)=>{
             if (index == this.props.calculator.history.length - 1){
               return (<Text style = {{fontSize: 25, color: '#E94B35', marginHorizontal: 5, marginVertical:3}}>
@@ -40,13 +47,17 @@ export default class Calculator extends Component {
                 {result}
               </Text>)
             }
-
           })}
-          <TextInput editable = {true} style = {{height: 30, borderColor: "black", borderWidth: 1, color: 'black'}} value = {this.state.expression} onChangeText = {(expression) => this.setState({expression})}/>
-
+          <View style = {{flexDirection: 'row', width: '100%'}}>
+            <Text style = {{height: 40, fontSize: 30, width: '5%'}}>
+              >
+            </Text>
+            <TextInput editable = {true} style = {{width: '95%', marginLeft: 10, height: 40, fontSize: 30, color: 'white'}} value = {this.state.expression} onChangeText = {(expression) => this.setState({expression})}>
+            </TextInput>
+          </View>
         </ScrollView>
-        <TouchableOpacity onPress = {()=>this._evaluateExpression.call(this)}>
-          <Text>Evaluate</Text>
+        <TouchableOpacity onPress = {()=>this._evaluateExpression.call(this)} style = {{height: '10%', width: '100%', justifyContent: 'center', alignItems: 'center', backgroundColor: '#FFC742'}}>
+          <Text style = {{fontSize: 30}}>Evaluate</Text>
         </TouchableOpacity>
       </View>
     )
